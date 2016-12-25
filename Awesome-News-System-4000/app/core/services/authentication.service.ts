7944f71d5ccc0b63151859a2ee2f9a0191811e8a
Milena Stancheva <mil.stancheva@gmail.com>
2016-12-25 16:08:18 +0200

    login fixed - localStorage saves data

--- Awesome-News-System-4000/app/core/services/authentication.service.ts
+++ Awesome-News-System-4000/app/core/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 const loginUrl = 'http://localhost:3001/user/login';
@@ -10,16 +10,22 @@ export class AuthenticationService {
     constructor(private http: Http) { }
 
     login(username: string, password: string) {
-        return this.http.post(loginUrl, JSON.stringify({ username: username, password: password }))
+        return this.http.post(loginUrl, JSON.stringify({ username: username, password: password }), this.setHeaders())
             .map((response: Response) => {
-                let user = response.json();
-                if (user && user.token) {
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                let apiResponse = response.json();
+
+                if (apiResponse && apiResponse.user.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(apiResponse));
                 }
             });
     }
 
     logout() {
         localStorage.removeItem('currentUser');
+    }
+
+    private setHeaders() {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return new RequestOptions({ headers: headers });
     }
 }
--- Awesome-News-System-4000/app/core/services/user.service.ts
+++ Awesome-News-System-4000/app/core/services/user.service.ts
@@ -11,22 +11,21 @@ export class UserService {
     constructor(private http: Http) { }
 
     getById(id: number) {
-        return this.http.get(apiUrl + '/user/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.get(apiUrl + '/user/' + id, this.createHeaderWithJwtToken()).map((response: Response) => response.json());
     }
 
     create(user: User) {
-        return this.http.post(apiUrl + '/user/register', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post(apiUrl + '/user/register', user, this.createHeaderWithJwtToken()).map((response: Response) => response.json());
     }
 
     update(user: User) {
-        return this.http.put(apiUrl + '/user/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+        return this.http.put(apiUrl + '/user/' + user.id, user, this.createHeaderWithJwtToken()).map((response: Response) => response.json());
     }
 
-    private jwt() {
-        // create authorization header with jwt token
+    private createHeaderWithJwtToken() {
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+            let headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + currentUser.token });
             return new RequestOptions({ headers: headers });
         }
     }
