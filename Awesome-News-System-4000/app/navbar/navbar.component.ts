be1023a0a1c54efce8c75493b71ee9ae9a7b27a9
mkjordanov <mkjordanov@gmail.com>
2016-12-27 21:18:42 +0200

    added small modifications to search.Still now working

--- Awesome-News-System-4000/app/navbar/navbar.component.ts
+++ Awesome-News-System-4000/app/navbar/navbar.component.ts
@@ -16,6 +16,7 @@ export class NavbarComponent {
         private authenticationService: AuthenticationService,
         private searchComponent: SearchComponent,
         private router :Router) {
+            
         this.searchPhrase='';
      }
 
